8a8546a39a838fca52a0a6cdf33058141a53c3be
lyleXX <740334078@qq.com>
2023-11-30 23:22:35 +0800

    init:项目搭建

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import routes from '@/router'
+
+function App() {
+
+  return (
+    <>
+        {
+            useRoutes(routes)
+        }
+
+    </>
+  )
+}
+
+export default App
---
+++ src/views/discover/Discover.tsx
@@ -0,0 +1,8 @@
+import React,{memo} from "react";
+
+const Discover = () =>{
+    return (
+        <div>Discover</div>
+    )
+}
+export default memo(Discover);
