99894f9185eb662b013a5993faad8da5a044a4b6
lyleXX <740334078@qq.com>
2023-12-01 23:19:13 +0800

    feat:实现拖拽功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
 import routes from "@/router";
+import wallpaper from '@/assets/images/wallpaper.jpg'
 
 function App() {
-  return <>{useRoutes(routes)}</>;
+  return <div style={{backgroundImage:`url(${wallpaper})`}} className='h-[100vh] w-[100vw]'>{useRoutes(routes)}</div>;
 }
 
 export default App;
--- src/views/discover/Discover.tsx
+++ src/views/discover/Discover.tsx
@@ -1,6 +1,33 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 
 const Discover = memo(() => {
-  return <div className="text-red-500">Discover</div>
+  useEffect(() => {
+    draggable()
+  }, [])
+
+  /***/
+  const draggable = () => {
+    const dragBox = document.querySelector('.dragBox') as HTMLDivElement
+    dragBox.addEventListener('mousedown', function (e) {
+      const disX = e.clientX - dragBox.offsetLeft
+      const disY = e.clientY - dragBox.offsetTop
+      document.onmousemove = function (e) {
+        const l = e.clientX - disX
+        const t = e.clientY - disY
+        dragBox.style.left = l + 'px'
+        dragBox.style.top = t + 'px'
+      }
+      document.onmouseup = function () {
+        document.onmousemove = null
+        document.onmouseup = null
+      }
+    })
+  }
+
+  return (
+    <div className="dragBox absolute left-[100px] top-[100px] h-[400px] w-[500px] bg-red-500">
+      Discover
+    </div>
+  )
 })
 export default Discover
